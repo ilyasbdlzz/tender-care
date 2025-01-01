51940c3741d9a6c2d563d7a3e5ca5e03f01b526b
HilmiKamil <hilmihelios@gmail.com>
2025-01-01 12:37:58 +0700

    disuruh ilyas part 2

--- routes/web.php
+++ routes/web.php
@@ -61,11 +61,6 @@ Route::get('/article', function () {
     return view('user/article');
 });
 
-
-Route::get('/', function () {
-    return view('user/index');
-})->middleware(['auth', 'verified'])->name('user');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
