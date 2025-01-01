ee5a75fab40ecc4f6aad7f03b89db37a67aa8c0b
HilmiKamil <hilmihelios@gmail.com>
2025-01-01 12:16:14 +0700

    Counseling

---
+++ app/Http/Controllers/CounselingController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CounselingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $growthh = GrowthRecord::all();
+        return view('user.growthh', compact('growthh'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $growthrecords = GrowthRecord::all();
+        return view('user.growthrecord', compact('growthrecords'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validasi form input
+        $validated = $request->validate([
+            'height' => 'required|string',
+            'weight' => 'required|string',
+            'head' => 'required|string',
+            'date' => 'required|string',
+        ]);
+        $validated['iduser'] = auth()->user()->id;
+
+        GrowthRecord::create($validated);
+        return redirect('/growthhistory')->with('pesan', 'Data Berhasil di Tambah');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_01_01_051152_create_counseling_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('counseling', function (Blueprint $table) {
+            $table->id(); // Auto-increment primary key
+            $table->string('iduser'); // Foreign key to User table
+            $table->string('idtenagamedis'); // Foreign key to MedicalProfessional table
+            $table->enum('Status', ['Pending', 'Approved', 'Rejected'])->default('Pending'); // Status field with default value
+            $table->date('Date'); // Counseling date
+            $table->timestamps(); // Created_at and Updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('counseling');
+    }
+};
